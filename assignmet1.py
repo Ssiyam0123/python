101c38ffd180c5a3eca785c505df7e4edc21fb57
Ssiyam0123 <ssiyam563@gmail.com>
2025-04-11 23:05:57 +0600

    some simple problem of python

---
+++ assignmet1.py
@@ -0,0 +1,11 @@
+sum = 0
+
+for i in range(0,101,2):
+    #if(i%2 == 0):
+        sum +=i
+        
+        
+        
+        
+        
+print("sum of even numbers from 1 to 100:",sum)
--- first.py
+++ first.py
@@ -1,1 +1,26 @@
-print("hello world")
+#print("hello world")
+#Check Even or Od
+num = int(input("Enter your number: "))
+
+if num %2 == 0 : print("Even number")
+else: print("Odd number")
+
+
+
+
+#simple calculator
+num1 = float(input("Enter the first number :"))
+operator = str(input("select any operators +,-,*,/ :"))
+
+num2 = float(input("Enter the second number: "))
+
+if operator == "+" : print("Result: ",num1+num2)
+elif operator =="-" : print("Result: ", num1-num2)
+elif operator=="*" : print("Result: ", num1*num2)
+elif operator=="/" : print("Result: ", num1/num2)
+else : print("invalid operation")
+
+
+
+
+ 
