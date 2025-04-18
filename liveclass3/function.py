0a047f451976a5174b073c13bef2993391d9461d
Ssiyam0123 <ssiyam563@gmail.com>
2025-04-18 20:02:11 +0600

    lambda function

--- liveclass3/function.py
+++ liveclass3/function.py
@@ -1,4 +1,29 @@
-def greet():
-    print("hello word")
+# def greet():
+#     print("hello word")
 
-greet()    
+# greet()    
+
+
+
+# def add(num1, num2):
+#     return num1+num2
+
+
+# hiSum = add(3,4.45)
+
+# print(hiSum)
+
+
+
+
+# add = lambda x,y : x+y
+
+
+# print(add(12,32))
+
+
+
+sum = lambda *args : sum(args)
+
+
+print(sum(1,2,3,4))
