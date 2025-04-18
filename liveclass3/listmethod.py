6250c0c3c620492d1405705f77f2c8bb7edca04d
Ssiyam0123 <ssiyam563@gmail.com>
2025-04-18 19:45:33 +0600

    function in python

--- liveclass3/listmethod.py
+++ liveclass3/listmethod.py
@@ -4,6 +4,13 @@
 
 # print(number)
 
+# output: [1, 2, 3, [7, 8]]
+
+
+
+
+
+
 
 # list1 = [1,2,3]
 # list2 = [7,8,9]
@@ -11,3 +18,73 @@
 # list1.extend(list2)
 
 # print(list1)
+# output: [1, 2, 3, 7, 8, 9]
+
+
+
+
+
+
+# f = ['a', 'b']
+
+# f.insert(1,'c')
+
+# print(f)
+# output: ['a', 'c', 'b']
+
+
+
+
+
+
+# a1 = [1,2,3]
+# b1=[4,6,7,8]
+# a1.extend(b1)
+
+# a1.insert(4,5)
+
+# print(a1)
+
+# output: [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+
+
+# number = [10,28,33,42,54,36,22]
+
+# number.remove(10)
+# print(number)
+
+
+# numbers = [1,2,3,4,5,3,2]
+
+# numbers.pop(4)
+# print(numbers)
+
+
+
+
+
+
+# a = [1, 2, 3, 4, 5, 2, 3]
+# b = [5,6,7]
+
+
+# for i in a:
+#     print(i)
+    # a.insert(1,i)
+    
+# print(a)    
+
+
+
+
+student = {
+    "name" : "john",
+    "age" : 20
+    }
+
+student.update({"age": 25})
+
+print(student)
+
