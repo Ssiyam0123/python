ed73f784bfe26c751671a65b9b0a1eafc4064558
Ssiyam0123 <ssiyam563@gmail.com>
2025-04-20 11:59:57 +0600

    assignment 2.2

---
+++ assignment2.2/problem1.py
@@ -0,0 +1,15 @@
+a = {1,2,3,4,5}
+b = {4,5,6,7,8}
+
+
+union = a | b
+
+intersection = a & b
+
+difference = b-a
+
+print("Union : ", union)
+
+print("Intersection : ", intersection)
+
+print("Difference : ", difference)
