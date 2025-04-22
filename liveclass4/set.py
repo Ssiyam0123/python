7524f0fb8e523e8a30090ca989ceea3edaee34a7
Ssiyam0123 <ssiyam563@gmail.com>
2025-04-22 19:24:15 +0600

    live class 4

---
+++ liveclass4/set.py
@@ -0,0 +1,17 @@
+fruits  = {"apple", "banana", "cherry", "banana"}
+
+print(fruits)
+
+fruits.add("naspati")
+
+print(fruits)
+
+
+# fruits.clear()
+# print(fruits)
+
+
+
+copy_fruits = fruits.copy()
+
+print(copy_fruits)
