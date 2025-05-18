a7b561782a3363bd6a2befda19e4f9b1eb189964
Ssiyam0123 <ssiyam563@gmail.com>
2025-05-18 23:33:29 +0600

    numpy

---
+++ numpy/first.py
@@ -0,0 +1,19 @@
+import numpy as np;
+
+my_list = [10,20,30,40]
+my_array = (50,60,70,80)
+my_matrix = [[1,2,3], [4,5,6], [7,8,9]]
+
+my_array2 = ([1,2,3],[4,5,6],[7,8,9])
+
+result = np.array(my_matrix)
+# print(result)
+
+output20_30 = np.arange(0,30,3)
+# print(output20_30)
+
+ip = np.eye(5)
+# print(ip)
+
+rando = np.random.seed(0)
+print(rando)
